c7a5f80ede62ee7456744fd0495ac97a4c75f442
easakibahmad <sakib.sgc.du@gmail.com>
2025-07-23 00:31:45 +0600

    23-07-25:00:31 add marque text

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,10 @@ function App() {
             apiKey={import.meta.env.VITE_DRIVE_API_KEY}
           />
         </CustomPopup>
-        
+
+        <div className="marquee-container absolute bottom-0 left-0 w-full z-[1001]">
+          <p className="marquee-text">  © Background Video by Jacob + Katie Schwarz — used for demonstration purposes only.</p>
+        </div>
       </div>
     </>
   )
