2bc8a03019423f9af3b8a09f19f3ea12b6c85603
easakibahmad <sakib.sgc.du@gmail.com>
2025-07-29 15:42:01 +0600

    29-07-25:15:41 update drive component

--- src/components/DriveImageFetcher.tsx
+++ src/components/DriveImageFetcher.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import type { DriveImageFetcherProps } from "../types/drive_image_fetcher";
 import { useDriveImages } from "../custom_hooks/drive_hook";
+import type { DriveImageFetcherProps } from "../types/drive";
 
 const DriveImageFetcher: React.FC<DriveImageFetcherProps> = ({ folderId, apiKey }) => {
   const { images, loading, error } = useDriveImages(folderId, apiKey);
@@ -14,7 +14,7 @@ const DriveImageFetcher: React.FC<DriveImageFetcherProps> = ({ folderId, apiKey 
       <h3 className="bg-yellow-300 rounded-md pl-2 my-6">Images in folder (iframe preview):</h3>
       {images.length === 0 && <p>No images found.</p>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 image-grid">
-        {images.map((file) => (
+        {images.map((file: { id: string }) => (
           <iframe
             key={file.id}
             src={`${import.meta.env.VITE_DRIVE_GOOGLE}/file/d/${file.id}/preview`}
