6bb20189982f6bef5d3df271e67403c8600c8b97
easakibahmad <sakib.sgc.du@gmail.com>
2025-08-03 23:23:46 +0600

    03-08-2024:11:23 update dAndD colors

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
+import { dAndDblockColors } from "@/constants/DandD";
 
 const Admin = () => {
-  const [blocks, setBlocks] = useState([
-    { id: "1", color: "bg-green-600" },
-    { id: "2", color: "bg-red-600" },
-    { id: "3", color: "bg-yellow-600" },
-    { id: "4", color: "bg-sky-600" },
-    { id: "5", color: "bg-emerald-600" },
-    { id: "6", color: "bg-cyan-600" },
-    { id: "7", color: "bg-indigo-600" },
-    { id: "8", color: "bg-violet-600" },
-  ]);
+  const [blocks, setBlocks] = useState(dAndDblockColors);
 
   const [draggingId, setDraggingId] = useState<string | null>(null);
 
