f5c440bf40594fa59a6305bc6deff91f547cc928
easakibahmad <sakib.sgc.du@gmail.com>
2025-08-04 22:55:27 +0600

    04-08-2025:10:55 change bg colors for dnd

--- src/constants/DandD.ts
+++ src/constants/DandD.ts
@@ -1,10 +1,10 @@
 export const dAndDblockColors = [
-    { id: "1", color: "bg-green-600" },
-    { id: "2", color: "bg-red-600" },
-    { id: "3", color: "bg-yellow-600" },
-    { id: "4", color: "bg-sky-600" },
-    { id: "5", color: "bg-emerald-600" },
-    { id: "6", color: "bg-cyan-600" },
-    { id: "7", color: "bg-indigo-600" },
-    { id: "8", color: "bg-violet-600" },
-  ]
+  { id: "1", color: "bg-green-600" },
+  { id: "2", color: "bg-red-600" },
+  { id: "3", color: "bg-yellow-600" },
+  { id: "4", color: "bg-blue-600" },
+  { id: "5", color: "bg-pink-600" },
+  { id: "6", color: "bg-cyan-600" },
+  { id: "7", color: "bg-indigo-600" },
+  { id: "8", color: "bg-violet-600" },
+];
